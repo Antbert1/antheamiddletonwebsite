1dbc4b860ca90df52241e6a10908d4f14bf203d8
Anthea <anthea4002@gmail.com>
2018-05-03 22:00:01 +0100

    Removed rogue echo

--- classes/Article.php
+++ classes/Article.php
@@ -81,7 +81,6 @@ class Article
   */
 
   public static function getList( $numRows=1000000, $order="publicationDate DESC" ) {
-    echo "GET LIST CALLED";
     $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
     $sql = "SELECT SQL_CALC_FOUND_ROWS *, UNIX_TIMESTAMP(publicationDate) AS publicationDate FROM articles
             ORDER BY " . mysql_escape_string($order) . " LIMIT :numRows";
